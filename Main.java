de3db0c631bc75d5218bd3ba535daacb30dbcea2
Kathleen McLead <katiemclead@gmail.com>
2023-02-15 13:07:53 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,21 @@
+class Main {
+  public static void main (String[] args) {
+   /*
+    String s1a = "String Example 1";
+    String s1b = "String Example 1";
+    System.out.println("== " + (s1a == s1b));
+    System.out.println(".equals " + s1a.equals(s1b));
+ */
+    /*
+    String s2a = new String("String Example 2");
+    String s2b = new String("String Example 2");
+    System.out.println("== " + (s2a == s2b));
+    System.out.println(".equals " + s2a.equals(s2b));
+
+    */
+/*
+    String s3 = "hi hello yo";
+    System.out.println(s3.indexOf("hello"));
+    */
+  }
+}
